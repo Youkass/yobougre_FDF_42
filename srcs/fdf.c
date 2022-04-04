9d08b39cd2ddac6797831eb7212b6c3a86131125
Hugo RECOLET <hrecolet@e1r12p23.42.fr>
2022-04-04 11:01:32 +0200

    [Update] Added av[1] open the map

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 16:23:14 by yobougre          #+#    #+#             */
-/*   Updated: 2022/04/04 10:36:41 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/04/04 11:00:44 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,25 +30,31 @@ void	ft_print(t_data data)
 	}
 }
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	mlx_data	data;
 	t_data		img;
 
-	data.mlx = mlx_init();
-	data.mlx_win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Wassup world");
-	img.img = mlx_new_image(data.mlx , WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
-	if (ft_fill_int_tab(&img.map, ft_split_line(ft_read_map("pyramide.fdf", &img.map))) < 0)
-		return (-1);
-	img.scale = 10;
-	img.coord = ft_parse_point(&img);
-	if (!img.coord)
-		return (-1);
-	ft_proj_point(&img);
-	//ft_print(img);
-	ft_draw_y(&img);
-	ft_draw_x(&img);
-	mlx_put_image_to_window(data.mlx, data.mlx_win, img.img, 0 , 0);
-	mlx_loop(data.mlx);
+	if (ac == 2)
+	{
+		data.mlx = mlx_init();
+		data.mlx_win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Wassup world");
+		img.img = mlx_new_image(data.mlx , WIDTH, HEIGHT);
+		img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
+		if (open(av[1], 0) < 0)
+			return (0);
+		if (ft_fill_int_tab(&img.map, ft_split_line(ft_read_map(av[1], &img.map))) < 0)
+			return (-1);
+		img.scale = 10;
+		img.coord = ft_parse_point(&img);
+		if (!img.coord)
+			return (-1);
+		ft_proj_point(&img);
+		//ft_print(img);
+		ft_draw_y(&img);
+		ft_draw_x(&img);
+		mlx_put_image_to_window(data.mlx, data.mlx_win, img.img, 0 , 0);
+		mlx_loop(data.mlx);
+	}
+	return (0);
 }
